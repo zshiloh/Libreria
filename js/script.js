e3e978efae9762f332c1d1b0408de36f9e494fa6
zshiloh <retoxdhajufere@gmail.com>
2024-08-16 10:42:40 -0500

    Update Libreria

--- js/script.js
+++ js/script.js
@@ -1,25 +1,40 @@
-let index = 1
-let slideImagenes = document.getElementsByClassName("carrusel")
+let index = 1;
+let slideImagenes = document.getElementsByClassName("carrusel fade");
+let puntos = document.getElementsByClassName("punto");
 
-slide(index)
+slide(index);
 
-function push(n){
-    slide(index += n)
+let timer = setInterval(() => push(1), 3500);
+
+function push(n) {
+    slide(index += n);
+    resetTimer();
 }
 
-function slide(n){
+function irASlide(n) {
+    slide(index = n);
+    resetTimer();
+}
 
-    if(n > slideImagenes.length){
-        index = 1
+function slide(n) {
+    if (n > slideImagenes.length) {
+        index = 1;
+    }
+    if (n < 1) {
+        index = slideImagenes.length;
     }
 
-    if(n < 1){
-        index = slideImagenes.length
+    for (let i = 0; i < slideImagenes.length; i++) {
+        slideImagenes[i].style.display = "none";
     }
+    for (let i = 0; i < puntos.length; i++) {
+        puntos[i].classList.remove("active");
+    }
+    slideImagenes[index - 1].style.display = "block";
+    puntos[index - 1].classList.add("active");
+}
 
-    //RECORRE TODAS IMAGENES
-    for(i=0; i<slideImagenes.length; i++){
-        slideImagenes[i].style.display= "none"
-    }
-    slideImagenes[index-1].style.display="block"
+function resetTimer() {
+    clearInterval(timer);
+    timer = setInterval(() => push(1), 3000);
 }
